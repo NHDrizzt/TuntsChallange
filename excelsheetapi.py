9a88699ed0fdf348e6ef0000f3d0d4df4b246716
Rokol <biel_s.carvalho2@hotmail.com>
2021-09-05 17:37:09 -0300

    Writing funcionality

--- excelsheetapi.py
+++ excelsheetapi.py
@@ -1,20 +1,33 @@
-from typing import ValuesView
+import gspread
+import time
 from googleapiclient.discovery import build
-
+from gspread.models import Worksheet
+from oauth2client.service_account import ServiceAccountCredentials
 from google.oauth2 import service_account
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SERVICE_ACCOUNT_FILE = 'key.json'
-SAMPLE_SPREADSHEET_ID = '1H03M4vYkeq4-ktwMvCHj6t-q5m5CJPZqDd40YOZ8RiA'
+scope = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/drive',]
+creds = ServiceAccountCredentials.from_json_keyfile_name('key.json', scope)
+client = gspread.authorize(creds)
 
-creds = None
-creds = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+gs = client.open_by_key("1H03M4vYkeq4-ktwMvCHj6t-q5m5CJPZqDd40YOZ8RiA").worksheet("engenharia_de_software")
+i=4 
+y=3
 
-service = build('sheets', 'v4', credentials=creds)
+while gs.cell(i,y).value != "":
+    time.sleep(7)
+    media = (int(gs.cell(i,y+1).value) + int(gs.cell(i,y+2).value) + int(gs.cell(i,y+1).value))/3
+    if(int(gs.cell(i,y).value) > 15):
+        gs.update_cell(i,y+4, "Reprovado por falta")
+        gs.update_cell(i, y+5, 0)
+    else: 
+        if(media<50):
+            gs.update_cell(i, y+4, "Reprovado por nota")
+            gs.update_cell(i, y+5, 0)
+        if(media >=50 and media < 70):
+            gs.update_cell(i, y+4, "Exame Final")
 
-# Call the GoogleSheet v4 API
-sheet = service.spreadsheets()
-result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range="engenharia_de_software!A4:H27").execute()
-values = result.get('values', [])
-print(values)
+        if(media > 70):
+            gs.update_cell(i, y+4, "Aprovado")
+            gs.update_cell(i, y+5, 0)
+    i=i+1
+
